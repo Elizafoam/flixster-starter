e810e15ba2d7e1bdf505bd6339ed92b2b21c029e
Elizabeth Johnson <elizabethjohnson@elizabe-ltmnwup.internal.salesforce.com>
2024-06-18 13:45:28 -0700

    remove unused files and add to readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
-
-import Header from './Components/Header/Header';
 import MovieList from './Components/MovieList/MovieList';
 import './App.css';
 
--- src/pages/Search.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect} from 'react';
-
-const Search = () => {
-    return(
-        <div></div>
-    )
-}
-
-export default Search;
